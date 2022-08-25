e7dd3230b1403cb41bef58a7c1f3bf209f6b05a7
Elisa López <104422045+Elilpvz@users.noreply.github.com>
2022-08-25 16:59:49 +0200

    Add files via upload

--- worldPopulation.sql
+++ worldPopulation.sql
@@ -1,31 +1,31 @@
-3. Consultas:
+--3. Consultas:
 
 SELECT DISTINCT year FROM population_years;
 
-4. Población de Gabon:
+--4. Población de Gabon:
 
 SELECT * FROM population_years
 WHERE country = 'Gabon';
 
-5. Países con menor población en el año 2005:
+--5. Países con menor población en el año 2005:
 
 SELECT country FROM population_years
 WHERE year = 2005
 ORDER BY population ASC
 LIMIT 10;
 
-6. Países con mas de 100 millones en el 2010:
+--6. Países con mas de 100 millones en el 2010:
 
 SELECT DISTINCT country FROM population_years
 WHERE year = 2010
 AND population > 100;
 
-7. Países que contienen en su nombre la palabra "islas":
+--7. Países que contienen en su nombre la palabra "islas":
 
 SELECT DISTINCT country FROM population_years
 WHERE country like "%islands%";
 
-8. Diferencia de población entre 2000 y 2010:
+--8. Diferencia de población entre 2000 y 2010:
 
 SELECT year, population FROM population_years
 WHERE country = 'Indonesia'
